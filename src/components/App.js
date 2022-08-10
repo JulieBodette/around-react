4389bb8d0b79351e75834001572908a2f6737c02
JulieBodette <jr3python@gmail.com>
2022-08-09 21:40:49 -0400

    Avatar popup now had the form set via children. this will allow each form of each popup to be different

--- src/components/App.js
+++ src/components/App.js
@@ -51,7 +51,33 @@ function App() {
           title="Update Profile Picture"
           name="edit-avatar"
           isOpen={isEditAvatarPopupOpen}
-        />
+        >
+          <form name="avatarimage" className="modal__form">
+            {/*form name was formerly avatarimage, will need to edit the code that
+          references the form name now using edit-avatar also id=
+          edit-avatar-modal
+            This modal uses placeholder , but the other one
+  uses value*/}
+            <input
+              className="modal__input-text"
+              type="url"
+              name="avatar"
+              placeholder="Image link"
+              id="avatar-link-input"
+              required
+            />
+            {/*type=url is needed for validation- it checks to make sure user
+            entered a url*/}
+            <span className="modal__error avatar-link-input-error">
+              error here
+            </span>
+            {/*set input values to the same text as the default values on the
+            page*/}
+            <button type="submit" className="modal__submit-button">
+              Save
+            </button>
+          </form>
+        </PopupWithForm>
       </div>
       {/*End page content*/}
 
--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,6 +1,6 @@
 import close from "../images/CloseIcon.svg";
 
-export function PopupWithForm({ title, name, isOpen }) {
+export function PopupWithForm({ title, name, isOpen, children }) {
   /*const modal = document.querySelector("#edit-avatar-modal");
     modal.classList.add("modal_open");*/
   return (
@@ -11,30 +11,7 @@ export function PopupWithForm({ title, name, isOpen }) {
           <img src={close} alt="X" />
         </button>
         <h2 className="modal__title">{title}</h2>
-        <form name="avatarimage" className="modal__form">
-          /*form name was formerly avatarimage, will need to edit the code that
-          references the form name now using edit-avatar also id=
-          edit-avatar-modal*/ /*This modal uses placeholder , but the other one
-          uses value*/
-          <input
-            className="modal__input-text"
-            type="url"
-            name="avatar"
-            placeholder="Image link"
-            id="avatar-link-input"
-            required
-          />
-          /*type=url is needed for validation- it checks to make sure user
-          entered a url*/
-          <span className="modal__error avatar-link-input-error">
-            error here
-          </span>
-          /*set input values to the same text as the default values on the
-          page*/
-          <button type="submit" className="modal__submit-button">
-            Save
-          </button>
-        </form>
+        {children}
       </div>
     </div>
   );
