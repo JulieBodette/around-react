a81288184adbc37d4b15d1650f2ebbdf906e7251
JulieBodette <jr3python@gmail.com>
2022-08-11 16:31:53 -0400

    Added code to ImagePopup.js

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -1,3 +1,16 @@
+import close from "../images/CloseIcon.svg";
+
 export function ImagePopup() {
-  return <div></div>;
+  return (
+    /*modal for the image popup*/
+    <div className="popup modal" id="image-popup">
+      <div className="popup__content">
+        <img className="popup__image" />
+        <button type="button" className="modal__close-button">
+          <img src={close} alt="X" />
+        </button>
+        <h2 className="popup__caption">insert caption here</h2>
+      </div>
+    </div>
+  );
 }
