62ca026a1e24d4f0c8fd723749b6c8f4a17098d9
JulieBodette <jr3python@gmail.com>
2022-08-22 21:05:10 -0400

    Made name and description state variables, made the input fields for them be controlled components

--- src/components/EditProfilePopup.js
+++ src/components/EditProfilePopup.js
@@ -1,6 +1,20 @@
 import { PopupWithForm } from "./PopupWithForm.js";
+import React, { useState } from "react";
 
 export function EditProfilePopup({ isOpen, onClose }) {
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+
+  function handleNameChange(e) {
+    setName(e.target.value);
+    console.log(name);
+  }
+
+  function handleDescriptionChange(e) {
+    setDescription(e.target.value);
+    console.log(description);
+  }
+
   return (
     <PopupWithForm
       title="Edit Profile"
@@ -10,27 +24,31 @@ export function EditProfilePopup({ isOpen, onClose }) {
     >
       {/*might need to mess with form name re:the brief and use edit-profile (the props.name) */}
       <form name="nameandtitle" className="modal__form">
+        {/*This isnput is a controlled component- the value is set to the name state value*/}
         <input
           className="modal__input-text"
           type="text"
           name="name"
-          // value="Jacques Cousteau"
+          value={name}
           minLength="2"
           maxLength="40"
           id="name-input"
           placeholder="Name"
+          onChange={handleNameChange}
           required
         />
         <span className="modal__error name-input-error">error here</span>
+        {/*This isnput is a controlled component- the value is set to the description state value*/}
         <input
           className="modal__input-text"
           type="text"
           name="title"
-          //value="Explorer"
+          value={description}
           minLength="2"
           maxLength="200"
           id="title-input"
           placeholder="About"
+          onChange={handleDescriptionChange}
           required
         />
         <span className="modal__error title-input-error">error here</span>
